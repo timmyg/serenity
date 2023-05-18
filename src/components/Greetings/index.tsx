f88bd6d97aabbfdf14b90dd90e2b571af6998a21
tim <timmyg13@gmail.com>
2023-05-17 22:05:28 -0400

    wip

--- src/components/Greetings/index.tsx
+++ src/components/Greetings/index.tsx
@@ -4,7 +4,9 @@ import { useEffect, useState } from 'react'
 import { Container, Image } from './styles'
 import { Badge } from '../Badge'
 import { Button } from '../Button'
-import Skeleton from '@mui/material/Skeleton'
+// import Skeleton from '@mui/material/Skeleton'
+// import ReportIcon from '@mui/icons-material/Report'
+import { Alert } from '@mui/material'
 
 export function Greetings() {
   // function handleSayHello() {
@@ -54,12 +56,28 @@ export function Greetings() {
         <Badge status={currentStatus} />
       ) : (
         hasGrantedPermissions === false && (
-          <Button
-            onClick={handleGrantPermission}
-            disabled={hasGrantedPermissions}
-          >
-            Grant Permission
-          </Button>
+          <>
+            <Alert severity="warning">
+              Serenity needs accessibility permissions to monitor, not record,
+              your keyboard and mouse activity. This is crucial for the optimal
+              functionality of the app, helping it adapt to your interactions.
+            </Alert>
+
+            {/* <p> */}
+            {/* more */}
+            {/* It's important to clarify that we do not track specific keys you press or the exact positions of your mouse movements. Instead, we only monitor the general activity status, such as whether the keyboard or mouse is being used or not. This allows our application to better adapt to your workflow and offer a more responsive and customized experience.
+
+We also want to assure you that your privacy is of utmost importance to us. None of the data related to keyboard or mouse activity is sent to any server or used for any purpose other than improving your user experience within the application.
+
+We are committed to maintaining your trust and protecting your privacy. Should you have any questions or concerns, please don't hesitate to reach out to us. */}
+            {/* </p> */}
+            <Button
+              onClick={handleGrantPermission}
+              disabled={hasGrantedPermissions}
+            >
+              Grant Permission
+            </Button>
+          </>
         )
       )}
     </Container>
